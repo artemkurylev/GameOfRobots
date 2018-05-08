7871d2976713ae28b4f4e0e0d62f406e53bcb71b
Artem Kurylev <artembonapart@gmail.com>
2018-05-08 21:15:35 +0200

    Добавлен класс GameView

---
+++ src/view/GameView.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+/*import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.KeyEvent;*/
+import gameofrobots.Model.GameModel;
+import java.awt.Color;
+//import java.awt.Dimension;
+import java.awt.event.KeyListener;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author 1
+ */
+public class GameView extends JPanel implements KeyListener{
+    
+// ------------------------------ Модель игры ------------------------------
+    private GameModel _model;
+    
+    
+    
+    public GameView(GameModel model){
+
+    }
+    
+    
+    
+}
