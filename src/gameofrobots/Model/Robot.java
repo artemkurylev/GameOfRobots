bbe4347099d47fce326c83faa065656bd57812e3
Artem Kurylev <artembonapart@gmail.com>
2018-05-14 09:19:23 +0200

    Добавдена реализация основных функций класса robot

--- src/gameofrobots/Model/Robot.java
+++ src/gameofrobots/Model/Robot.java
@@ -5,10 +5,54 @@
  */
 package gameofrobots.Model;
 
+import gameofrobots.navigation.Direction;
+import gameofrobots.navigation.CellPosition;
+import gameofrobots.navigation.MiddlePosition;
 /**
  *
  * @author 1
  */
 public class Robot {
+    private CellPosition cellPosition;
+    private GameField Field;
+    private int stun; 
+    
+    
+    public CellPosition position(){
+        return this.cellPosition.clone();
+    };
+    public boolean setPosition(CellPosition Cell){
+        if(Cell.isValid()){
+            this.cellPosition = Cell;
+            return true;
+        }
+        return false;
+    }
+    
+    public int getStun(){
+        return stun;
+    }
+    public void setStun(int stn){
+        stn = stun;
+    }
+    public void reduceStun(){
+        stun--;
+    }
+    
+    public GameField Field(){
+        return this.Field;
+    }
+    public void makeMove(Direction direction){
+        if(!(Field.isWall(new MiddlePosition(this.position(),direction)))){
+            this.setPosition(this.position().next(direction));
+        }
+    }
+    
+    public void setPontoon(Direction direction){
+        if(this.Field.isBog(this.position()) && this.Field.isWall(new MiddlePosition(this.position(),direction))){
+            this.Field.setPontoon(this.position().next(direction));
+        }
+    }
+    
     
 }
