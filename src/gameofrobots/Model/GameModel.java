22cc623e62df2f1bf04d3e3d8f023eedeeec48e0
Artem Kurylev <artembonapart@gmail.com>
2018-05-14 21:43:01 +0200

    Добавлено условие конца игры

--- src/gameofrobots/Model/GameModel.java
+++ src/gameofrobots/Model/GameModel.java
@@ -4,18 +4,44 @@
  * and open the template in the editor.
  */
 package gameofrobots.Model;
+
+import gameofrobots.navigation.CellPosition;
+
 /**
  *
  * @author 1
  */
 public class GameModel {
-    GameField Field;
-    
+    private GameField Field;
+    private BigRobot bigRobot;
+    private SmallRobot smallRobot;
+    private CellPosition _targetPos;
     public GameModel(){
         Field = new GameField();
+        bigRobot = new BigRobot();
+        smallRobot = new SmallRobot();
+    }
+    public void start(){
+        Field = this.loadField();
     }
     public GameField field(){
         return this.Field;
     }
+
+    private GameField loadField() {
+    }
+    private void identifyGameOver(){
+        
+        if(smallRobot.position().equals(_targetPos))
+        {
+            System.out.println("Ты выйграл, красавчие");
+        }
+        if(bigRobot.position().equals(smallRobot.position()))
+        {
+            System.out.println("Ха ха, ты проиграл :DDDDDD");
+        }
+
+    }
     
+   
 }
